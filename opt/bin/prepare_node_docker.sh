213f4b0c228ba54f94f21277f468677069c10e28
Edward Raigosa <wenlock@hp.com>
2014-09-07 15:17:53 -0700

    Update prepare_node_docker.sh

--- opt/bin/prepare_node_docker.sh
+++ opt/bin/prepare_node_docker.sh
@@ -17,28 +17,44 @@
 # 
 # Test install:
 # curl https://raw.githubusercontent.com/wenlock/myhome/master/opt/bin/prepare_node_docker.sh | sudo bash -xe
+export AS_ROOT=${AS_ROOT:-0}
+function DO_SUDO {
+  if [ $AS_ROOT -eq 0 ] ; then
+    sudo $@
+  else
+    $@
+  fi
+}
+
+GIT_HOME=${GIT_HOME:-/opt/config/puppet/git}
+REVIEW_SERVER=${GIT_HOME:-https://review.forj.io}
 
 # prepare a node with docker installed
 # * we need puppet commandline setup, along with expected modules
-if [ ! $(id -u) -eq 0 ] ; then
-  echo "ERROR : SCRIPT should be run as sudo or root"
+if [ ! $(id -u) -eq 0 ] && [ $AS_ROOT -eq 1 ] ; then
+  echo "ERROR : SCRIPT should be run as sudo or root with export AS_ROOT=1"
   exit 1
 fi
 #
 # setup the basics
-apt-get update
-apt-get -y install git vim curl wget python-all-dev
-mkdir -p /opt/config/puppet/git
-cd /opt/config/puppet/git
+DO_SUDO apt-get update
+DO_SUDO apt-get -y install git vim curl wget python-all-dev
+mkdir -p "$GIT_HOME"
+cd "$GIT_HOME"
 git config --global http.sslverify false
-git clone https://review.forj.io/forj-oss/maestro
+git clone $REVIEW_SERVER/forj-oss/maestro
 cd maestro/
 bash -xe puppet/install_puppet.sh 
 bash -xe puppet/install_modules.sh
 
 #
 # install docker with puppet modules
-cd /opt/config/puppet/git
+cd "$GIT_HOME"
 git config --global http.sslverify false
-git clone https://review.forj.io/p/forj-config
+git clone $REVIEW_SERVER/p/forj-config
 cd forj-config/
+
+#
+# install docker
+PUPPET_MODULES=$GIT_HOME/forj-config:$GIT_HOME/maestro/puppet:/etc/puppet/modules
+DO_SUDO puppet apply --modulepath=$PUPPET_MODULES -e "include docker_wrap::requirements"
