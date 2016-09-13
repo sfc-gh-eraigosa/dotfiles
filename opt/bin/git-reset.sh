591e396e2b8318b2415d0e5af16737d072829d89
Edward Raigosa <edward.raigosa@hpe.com>
2016-09-13 22:16:24 +0000

    add some output and fix error
    Signed-off-by: Edward Raigosa <edward.raigosa@hpe.com>

--- opt/bin/git-reset.sh
+++ opt/bin/git-reset.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-upstream_remote=$(git remote -v|grep -v wenlock|grep '(push)'|awk '{print $1}'
-HewlettPackard)
+upstream_remote=$(git remote -v|grep -v wenlock|grep '(push)'|awk '{print $1}')
 
 if [ -z "${upstream_remote}" ]; then
   echo "ERROR no remote for upstream: $upstream_remote"
@@ -20,9 +19,14 @@ if [ ! -z "$1" ]; then
   _BRANCH=$1
 fi
 
+echo "Reseting $upstream_remote to $_BRANCH ..." && \
 git reset --hard ${upstream_remote}/$_BRANCH && \
+echo "Pulling $upstream_remote $_BRANCH ..." && \
 git pull ${upstream_remote} $_BRANCH && \
+echo "Reseting $upstream_remote to $_BRANCH ..." && \
 git reset --hard ${upstream_remote}/$_BRANCH && \
+echo "Pushing updates $ssh_remote to $_BRANCH ..." && \
 git push ${ssh_remote} $_BRANCH -f && \
 git pull origin $_BRANCH && \
+echo "Reseting $upstream_remote to $_BRANCH ..." && \
 git reset --hard ${upstream_remote}/$_BRANCH
