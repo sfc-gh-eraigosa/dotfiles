8eab687b716e6505701317fa171889f11ab0193a
Edward Raigosa <edward.raigosa@snowflake.com>
2024-04-02 08:43:20 -0700

    small script updates

--- opt/bin/git-rm-mybranches.sh
+++ opt/bin/git-rm-mybranches.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
+# set -x -v
+
 # clean up the current users branches
 set -e
-export MY_USER="${MY_USER:-$(whoami)}"
+export SEARCH_TERM="${SEARCH_TERM:-}"
+
 
 # make sure before moving forward
-if [ ! "$1" = "--force" ]; then
-  echo "Current directory $(pwd) and repo $(git rev-parse --show-toplevel)"
-  echo -n "Would you like to cleanup ($(printf '%s' $(git branch -l 2>&1 |grep "^\s\s${MY_USER}\/.*"|wc -l))) all branches? [Y]:" && \
-    read -r cleanup
-else
+if [ "$1" = "--force" ]; then
   cleanup="Y"
 fi
+
+if [ "${cleanup}" = "Y" ]; then
+    SEARCH_TERM="${2}"
+fi
+
+if [ -n "${1}" ]; then
+    SEARCH_TERM="${1}"
+fi
+
+if [ -z "${SEARCH_TERM}" ]; then
+    echo "Usage: $o [--force] regex"
+    exit 1
+fi
+
+if [ ! "${cleanup}" = "Y" ]; then
+  echo "Current directory $(pwd) and repo $(git rev-parse --show-toplevel)"
+  echo -n "Would you like to cleanup ($(printf '%s' $(git branch -l 2>&1 |grep "^\s\s${SEARCH_TERM}"|wc -l))) all branches? [Y]:" && \
+    read -r cleanup
+fi
+
 # this depends on branches starting with the logged in users user name
-for b in $(git branch -l 2>&1 |grep "^\s\s${MY_USER}\/.*"); do
+for b in $(git branch -l 2>&1 |grep "^\s\s${SEARCH_TERM}"); do
   echo "Cleaning up branch $b";
   if [ "${cleanup}" = "Y" ]; then
     git branch -D $b
