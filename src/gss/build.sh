984803399f05a5530fd09425cdd7a049637a8cd7
Edward Raigosa <edward.raigosa@gmail.com>
2026-05-10 16:32:04 -0700

    fix(gss): use HOME variable for BIN_DIR in build script

--- src/gss/build.sh
+++ src/gss/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-BIN_DIR="/home/wenlock/opt/bin"
+
+# Use HOME variable to define the binary directory
+BIN_DIR="${HOME}/opt/bin"
+
 mkdir -p "$BIN_DIR"
 echo "Building gss..."
 go build -o "$BIN_DIR/gss" main.go
